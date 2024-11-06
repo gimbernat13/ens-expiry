b586a950b3e35c37d3d58e51b24ebe1e198562be
Diego <gimbernat13@gmail.com>
2024-11-05 23:15:45 -0600

    ngrok

--- apollo-client.ts
+++ apollo-client.ts
@@ -12,10 +12,8 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
     cache: new InMemoryCache(),
     link: new HttpLink({
       // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: 'http://localhost:4350/graphql',
-      // you can disable result caching here if you want to
-      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-      // fetchOptions: { cache: "no-store" },
+      uri: 'https://ce00-2806-104e-c-624e-7184-a2a7-46ed-4ed5.ngrok-free.app/graphql',
+ 
     }),
   });
 });
